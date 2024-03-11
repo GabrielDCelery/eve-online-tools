eb0d0b31d0433e6a82cfd0af26ed8ed36595cc54
GaborZeller <gabriel.d.celery@gmail.com>
2024-03-11 19:18:06 +0000

    Moved docker folder to .docker folder

--- .dev/pre-commit.sh
+++ .dev/pre-commit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -f $(pwd)/docker/Dockerfile.precommit -t eve-online-tools-cli/precommit .
+docker build -f $(pwd)/.docker/Dockerfile.precommit -t eve-online-tools-cli/precommit .
 docker run --rm -t -e "TERM=xterm-256color" -v $(pwd):/home/local-user/eve-online-tools-cli eve-online-tools-cli/pre-commit
